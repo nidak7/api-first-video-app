98cc1a7b6f3a86346ac02a4ffe31db7c67bcce24
knida <knida8501@gmail.com>
2026-01-30 01:04:39 +0530

    Bind Flask app to Render PORT

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from extensions import init_db
 from flask_jwt_extended import JWTManager
+import os
 
 def create_app():
     app = Flask(__name__, static_folder="static", static_url_path="/static")
