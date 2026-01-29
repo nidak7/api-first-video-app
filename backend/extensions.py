b1acc05011b0eb2ec5af57287852a44f7c11dd3f
knida <knida8501@gmail.com>
2026-01-30 00:11:20 +0530

    Use MongoDB Atlas via env variable

--- backend/extensions.py
+++ backend/extensions.py
@@ -6,7 +6,7 @@ db = None
 
 def init_db():
     global mongo_client, db
-    mongo_uri = os.environ.get("MONGO_URI")
+    mongo_uri = os.getenv("MONGO_URI")
     mongo_client = MongoClient(mongo_uri)
     db = mongo_client["video_app"]
     print("MongoDB connected:", db.name)
